44351c7658f9fbe8ed45e60009194f946534272a
nenananana <160021396+nenananana@users.noreply.github.com>
2024-11-15 15:25:52 +0500

    Add files via upload

---
+++ ListOperations.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    internal class ListOperations
+    {
+        public static List<T> GetIntersection<T>(List<T> list1, List<T> list2)
+        {
+            HashSet<T> set1 = new HashSet<T>(list1);
+            HashSet<T> set2 = new HashSet<T>(list2);
+            set1.IntersectWith(set2);
+            return set1.ToList();
+        }
+
+        public static void AddReverseList<T>(LinkedList<T> list)
+        {
+            LinkedList<T> reverseList = new LinkedList<T>();
+
+         
+            foreach (var item in list)
+            {
+                reverseList.AddFirst(item);
+            }
+
+            foreach (var item in reverseList)
+            {
+                list.AddLast(item);
+            }
+        }
+
+        public static HashSet<string> AllStudents(List<HashSet<string>> studentTrcs)
+        {
+            HashSet<string> visitedByAll = new HashSet<string>(studentTrcs[0]);
+            foreach (var trcs in studentTrcs.Skip(1))
+            {
+                visitedByAll.IntersectWith(trcs);
+            }
+
+            return visitedByAll;
+        }
+        public static HashSet<string> SomeStudents(List<HashSet<string>> studentTrcs)
+        {
+            var visitedBySome = new HashSet<string>();
+            foreach (var trcs in studentTrcs)
+            {
+                visitedBySome.UnionWith(trcs);
+            }
+
+            return visitedBySome;
+        }
+        public static HashSet<string> NotVisited(List<string> allTrcs, List<HashSet<string>> studentTrcs)
+        {
+            var allVisited = new HashSet<string>(SomeStudents(studentTrcs));
+            var notVisited = new HashSet<string>(allTrcs);
+            notVisited.ExceptWith(allVisited);
+
+            return notVisited;
+        }
+
+        public static HashSet<char> CountLetters(string filePath)
+        {
+            HashSet<char> uniqueLetters = new HashSet<char>();
+
+            try
+            {
+                // Открываем файл для чтения
+                using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                using (StreamReader reader = new StreamReader(fileStream, Encoding.UTF8))
+                {
+                    int character;
+                    // Читаем файл посимвольно
+                    while ((character = reader.Read()) != -1)
+                    {
+                        char ch = Convert.ToChar(character);
+                        // Добавляем символ
+                        if (IsCyrillic(ch))
+                        {
+                            uniqueLetters.Add(char.ToLower(ch));
+                        }
+
+                    }
+                }              
+            }
+            catch
+            {
+                Console.WriteLine("Файл не найден.");
+            }
+            return uniqueLetters;
+        }
+        // Метод для проверки, является ли символ буквой кириллицы
+        private static bool IsCyrillic(char ch)
+        {
+            return (ch >= 'А' && ch <= 'я') ;
+        } 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,139 @@
+﻿using lab4;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Numerics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+class Program
+{
+    static void Main()
+    {
+        string ex;
+
+        Console.WriteLine("Выберете номер задания: \n\n" +
+            "1.  Задание 1.\n" +
+            "2.  Задание 2.\n" +
+            "3.  Задание 3.\n" +
+            "4.  Задание 4.\n" );
+        ex = Console.ReadLine();
+        switch (ex)
+        {
+            case "1":
+                Console.WriteLine("Формирует список L, включив в него по одному разу элементы, которые входят одновременно в оба списка L1 и L2.");
+                List<string> L1 = new List<string>();
+                Console.WriteLine("Введите через пробел элементы списка L1");
+                string inputL1 = Console.ReadLine();
+                string[] splitWordsL1 = inputL1.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string word in splitWordsL1)
+                {
+                    L1.Add(word);
+                }
+
+                List<string> L2 = new List<string>();
+                Console.WriteLine("Введите через пробел элементы списка L2");
+                string inputL2 = Console.ReadLine();
+                string[] splitWordsL2 = inputL2.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string word in splitWordsL2)
+                {
+                    L2.Add(word);
+                }
+                List<string> intersection = ListOperations.GetIntersection(L1, L2);
+                if (intersection.Count() == 0)
+                {
+                    Console.WriteLine("В списках нет одинаковых элементов");
+                }
+                else
+                {
+                    Console.WriteLine("Пересечения: " + string.Join(", ", intersection));
+                }
+                
+                break;
+            case "2":
+                Console.WriteLine("В конец непустого списка L добавить все его элементы, располагая их в обратном порядке");
+                LinkedList<string> L3 = new LinkedList<string>();
+                string inputL3;
+                do
+                {
+                    Console.WriteLine("Введите через пробел элементы списка. Список не должен быть пустым");
+                    inputL3 = Console.ReadLine();
+                    string[] splitWordsL3 = inputL3.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (string word in splitWordsL3)
+                    {
+                        L3.AddLast(word);
+                    }
+                } while (string.IsNullOrWhiteSpace(inputL3)) ;
+
+
+                ListOperations.AddReverseList(L3);
+                Console.WriteLine(string.Join(", ", L3));
+                break;
+            case "3":
+                try
+                {
+                    List<string> allTrcs = new List<string> { "ТЦ1", "ТЦ2", "ТЦ3", "ТЦ4", "ТЦ5" };
+                    Console.WriteLine("Сколько студентов в группе: ");
+                    int studentCount = Convert.ToInt32(Console.ReadLine());
+                    List<HashSet<string>> studentTrcs = new List<HashSet<string>>();
+                    Console.WriteLine("Список ТЦ: " + string.Join(", ", allTrcs));
+                    for (int i = 0; i < studentCount; i++)
+                    {
+                        HashSet<string> collection = new HashSet<string>();
+                        Console.WriteLine($"Введите ТЦ из списка для студента {i + 1} (введите '0' для завершения ввода):");
+
+                        while (true)
+                        {
+                            string input = Console.ReadLine();
+                            if (input.ToLower() == "0")
+                            {
+                                break;
+                            }
+
+                            if (allTrcs.Contains(input))
+                            {
+                                collection.Add(input);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Введенные данные не соответствуют списку. Попробуйте снова.");
+                            }
+                        }
+
+                        studentTrcs.Add(collection);
+                    }
+
+                    Console.WriteLine("\nСписок студентов:");
+                    for (int i = 0; i < studentTrcs.Count; i++)
+                    {
+                        Console.WriteLine($"Студент {i + 1}: {string.Join(", ", studentTrcs[i])}");
+                    }
+                   
+                    var visitedAll = ListOperations.AllStudents(studentTrcs);
+                    var visitedSome = ListOperations.SomeStudents(studentTrcs);
+                    var notVisited = ListOperations.NotVisited(allTrcs, studentTrcs);
+
+                    Console.WriteLine("ТЦ, которые посещали все студенты: " + string.Join(", ", visitedAll));
+                    Console.WriteLine("ТЦ, которые посещали некоторые студенты: " + string.Join(", ", visitedSome));
+                    Console.WriteLine("ТЦ, которые не посещал ни один студент: " + string.Join(", ", notVisited));            
+                }
+                catch
+                {
+                    Console.WriteLine("Некорректный ввод");
+                }
+                break;
+            case "4":
+                string filePath = "file.txt";
+                HashSet<char> uniqueLetters = ListOperations.CountLetters(filePath);
+
+                Console.WriteLine($"Количество букв в тексте: {uniqueLetters.Count}");
+                Console.WriteLine("Буквы: " + string.Join(", ", uniqueLetters));
+                
+                break;
+            case "5":
+                
+                break;
+            default:
+                Console.WriteLine("Такого номера нет или вы нажали лишний раз на пробел.");
+                break;
+        }     
+    }
+}
